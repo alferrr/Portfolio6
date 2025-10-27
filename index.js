25b050fa28b4daf885da37b6eecfc5d11eb5e504
Alfer Brent Mercado <alfzmercado240@gmail.com>
2025-10-27 17:03:53 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// slider
+
+new Swiper(".card", {
+  loop: true,
+  spaceBetween: 30,
+  centeredSlides: true, // Enable centered slides
+
+  autoHeight: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+
+    768: {
+      slidesPerView: 2,
+    },
+
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
